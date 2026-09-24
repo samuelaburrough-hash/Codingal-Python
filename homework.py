57d6e3045fb9c2a8b2d13ad246007a0689e8dee3
Noli <samuelaburrough@gmail.com>
2026-09-24 19:02:20 +0200

    homework.Python1

---
+++ homework.py
@@ -0,0 +1,4 @@
+print("I wake up at 7 AM and brush my teeth before going down to eat a delicious omelette made by my dad")
+print ("At 8:15 AM i go to school")
+print ("at school i we do math after a short time in where we tell what what we did the day before after school")
+print ("Then we do English for a while and singing lessons before the day ends")
